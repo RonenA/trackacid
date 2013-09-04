1b270ab12a757df3b0ee158290a63fe1ab3af42f
Ronen Ackerman <ronen.ackerman@gmail.com>
2013-09-04 18:44:45 -0400

    Figured out how to make devise respond to json

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,28 +2,4 @@ class RegistrationsController < Devise::RegistrationsController
 
   respond_to :html, :xml, :json
 
-  def create
-    build_resource
-
-    if resource.save
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_up(resource_name, resource)
-        respond_with resource, :location => after_sign_up_path_for(resource)
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-        expire_session_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords resource
-      respond_with resource
-    end
-  end
-
-  # Signs in a user on sign up. You can overwrite this method in your own
-  # RegistrationsController.
-  def sign_up(resource_name, resource)
-    sign_in(resource_name, resource)
-  end
 end
